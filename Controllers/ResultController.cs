0227c2e58b301f1082093e805ea003f5fb2a5046
Emma <emma.pearlman@gmail.com>
2020-01-21 21:22:48 +0000

    Allow selection of file

--- Controllers/ResultController.cs
+++ Controllers/ResultController.cs
@@ -1,54 +1,67 @@
-﻿
-using LeagueTable.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace LeagueTable.Controllers
+﻿namespace LeagueTable.Controllers
 {
+    using LeagueTable.Models;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Web.Mvc;
+
     public class ResultController : Controller
     {
 
-     [HttpGet]
+        [HttpGet]
         public ActionResult Index()
         {
+            string resultsPath = Server.MapPath("~/App_Data/uploads");
+            DirectoryInfo resultsDirectory = new DirectoryInfo(resultsPath);
+            var fileList = resultsDirectory.GetFiles("*.csv");
+            var files = new List<SelectListItem>();
+            foreach (var f in fileList)
+            {
+                files.Add(new SelectListItem
+                {
+                    Value = f.FullName,
+                    Text = f.Name
+                });
+            }
+
+            Session["resultsfiles"] = files;
             return View();
         }
-        
+
         [HttpPost]
         public ViewResult Submit()
         {
             //process data
 
             var pd = new ProcessData();
-            pd.uploadPath = Server.MapPath("~/App_Data/uploads");
-            pd.fileName = "PL-2015-2016.csv";
+            FileInfo newFile = new FileInfo(Request.Form["MyFiles"].ToString());
+            Session["FilePath"] = Request.Form["MyFiles"].ToString();
+            pd.uploadPath = newFile.DirectoryName;
+            pd.fileName = newFile.Name;
             var myLeague = pd.ProcessLeagueData();
 
-           
-            return View("ShowResults",myLeague);
+            return View("ShowResults", myLeague);
         }
 
         public ActionResult ShowResults(Models.LeagueTable myLeague)
         {
-            
-            return View("ShowResults",myLeague);
+
+            return View("ShowResults", myLeague);
         }
 
         public ActionResult ShowTeamResults(string team)
         {
             ViewBag.Message = "Results for " + team;
             var pd = new ProcessData();
-            pd.uploadPath = Server.MapPath("~/App_Data/uploads");
-            pd.fileName = "PL-2015-2016.csv";
+            FileInfo newFile = new FileInfo(Session["FilePath"].ToString());
+            pd.uploadPath = newFile.DirectoryName;
+            pd.fileName = newFile.Name;
             var myResults = pd.GetTeamResults(team);
-            return View("ShowTeamResults",myResults);
+            return View("ShowTeamResults", myResults);
         }
 
         public ViewResult GoBack()
-
         {
             return View("Index");
         }
